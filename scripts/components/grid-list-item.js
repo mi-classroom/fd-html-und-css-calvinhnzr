6801895d27e465cc1b319d66418957a0b3ae6797
calvinhnzr <calvinhinzer@gmail.com>
2024-05-01 01:13:55 +0200

    [FEATURE] fetch data from db.json

--- scripts/components/grid-list-item.js
+++
@@ -1,9 +0,0 @@
-let item = document.querySelectorAll('.grid-list-item')
-  
-item.forEach((item) => {
-    item.innerHTML = `
-    <h3>${item.dataset.title}</h3>
-    <p>${item.dataset.author}</p>
-`
-})
-
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,42 @@
-import "./components/grid-list-item.js"
+const HOST = 'http://localhost'
+const PORT = '5173'
 
+async function fetchData() {
+  try {
+    const response = await fetch(`${HOST}:${PORT}/db.json`)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+}
 
+const template = (data) => `
+  <li>
+    <figure>
+      <img src="${data.teaserImg}" alt="${data.title}" />
+      <figcaption>
+        <h3>${data.title}</h3>
+        <address>${data.author}</address>
+        <ul class="tag-list">
+          ${data.tags.keywords.map((tag) => `<li>${tag}</li>`).join('')}
+        </ul>
+      </figcaption>
+    </figure>
+  </li>
+`
+
+async function renderData() {
+  const container = document.querySelector('#article-container')
+  const data = await fetchData()
+
+  if (!data) {
+    return
+  }
+
+  data.articles.forEach((item) => {
+    container.innerHTML += template(item)
+  })
+}
+
+renderData()
